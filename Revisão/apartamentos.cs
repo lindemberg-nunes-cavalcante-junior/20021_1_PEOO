c346c367dd3ab79f3b150665ad1cd9f04138b5b9
GC jim <48134739+lindemberg-nunes-cavalcante-junior@users.noreply.github.com>
2022-01-05 00:57:44 +0000

    mudanças

---
+++ Revisão/apartamentos.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace Aulão_de_CSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Apto x = new Apto();
+            x.SetNumAndares(3);
+            x.SetNumAptosAndar(3);
+            Console.WriteLine($"{x.GetNumeros()}");
+
+        }
+    }
+    class Apto
+    {
+        private int numAndares;
+        private int numAptosAndar;
+        public void SetNumAndares(int i)
+        {
+            if (i > 0) numAndares = i;
+        }
+        public void SetNumAptosAndar(int i)
+        {
+            if (i > 0) numAptosAndar = i;
+        }
+        public int[] GetNumeros()
+        {
+            int[] numerodosapartamentos = new int[numAptosAndar * numAndares];           
+            int apartamentos = 1;
+            int numero;
+            int auxiliar = 1;
+            int b = 0;
+            for (int i = 1; i <= numAndares; i++)
+            {
+                auxiliar = 1;
+                for (int a = 1; a <= numerodosapartamentos.Length; a++)
+                {
+
+                    numero = apartamentos * 100 + auxiliar;
+                    numerodosapartamentos[b] = numero;
+                    auxiliar++;
+                    b++;
+                }
+                
+                
+                apartamentos++;
+            }
+            return numerodosapartamentos;
+        }
+    }
+}
